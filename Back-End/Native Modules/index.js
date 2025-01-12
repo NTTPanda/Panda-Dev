2381a72aa0d02604eed9019be5e231cd4e273752
NTTPanda <rohitkrishnagouda@gmail.com>
2025-01-12 10:23:27 +0530

    I wanted to push the code

---
+++ Back-End/Native Modules/index.js
@@ -0,0 +1,8 @@
+// Import the sillyname package
+const sillyname = require('sillyname');
+
+// Generate a random silly name
+const name = sillyname();
+
+// Print the silly name to the console
+console.log(`Your generated silly name is: ${name}`);
